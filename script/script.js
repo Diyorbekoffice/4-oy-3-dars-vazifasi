996171e149b088610e28df001e3471f75efe4ab5
Diyorbekoffice <diyorbekboymirzayev0@gmail.com>
2024-07-20 03:09:02 +0500

    new

---
+++ script/script.js
@@ -0,0 +1,193 @@
+// ___________/ If ga oid masalalar \_____________
+
+// // 1-masala
+// let yosh = 15;
+// let res = yosh >= 18 ? console.log('Siz katta yoshdagisiz') : console.log('Siz hali yoshsiz');
+
+// // 2-masala
+// let son = 16;
+// let res = son % 2 === 0 ? console.log('juft son') : console.log('toq son');
+
+// // 3-masala
+// let son = -16;
+// let res = son >=0 ? console.log('musbat son') : console.log('manfiy son');
+
+// // 4-masala
+// let temp = 16;
+// let res = temp > 20 ? console.log('Yengil kiying') : console.log('Qalin kiying');
+
+// // 5-masala
+// let ball = 95;
+// if (ball >= 90) {
+//     console.log("A'lo");
+// } else if(ball < 89 && ball >= 70){
+//     console.log("Yaxshi");
+// } else if(ball < 69 && ball >= 50){
+//     console.log("Qoniqarli");
+// } else 
+//     console.log("Yomon");
+
+// // 6-masala
+// let nameGuest = "Bek";
+// let res = nameGuest == "Ali" ? console.log("Salom Ali!") : console.log("Salom mehmon");
+
+// // 7-masala
+// let sum = 120;
+// let res = sum >100 ? console.log("Chegirma qo'llanildi") : console.log("Chegirma mavjud emas");
+
+
+
+
+// __________________/ Ternary ga oid masalalar \_________________
+// // 1-masala
+// let temp = 28;
+// let res = temp > 25 ? console.log("Issiq") : console.log("Sovuq");
+
+// // 2-masala 
+// let user = 70;
+// let res = user > 50 ? console.log("O'tdi") : console.log("Yiqildi");
+
+// // 3-masala
+// let num = 10;
+// let res = num > 0 ? console.log("Musbat") : console.log("Musbat");
+
+
+
+// ________________/ For ga oid masalalar \________________
+// // 1-masala
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// // 2-masala
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// // 3-masala
+// for (let i = 2; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+// // 4-masala
+// for (let i = 1; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+// // 5-masala
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//     console.log(sum += i);
+// }
+// console.log("1dan 10gacha sonlar yig'indisi: " + sum);
+
+// // 6-masala
+// let sum = 1;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(sum *= i);
+// }
+// console.log("1dan 10gacha sonlar ko'paytmasi: " + sum);
+
+// // 7-masala
+// let num = 5;
+// let fak = 1
+// for (let i = 1; i <= num; i++) {
+//     console.log(fak *= i);
+// }
+// console.log("sonning faktorial: ", fak);
+
+// // 8-masala
+// let num = 1;
+// for (let i = 0; i <= 50; i++) {
+//     console.log(num = i * i);        
+// }
+
+// // 9-masala
+// let num = 1;
+// for (let i = 0; i <= 20; i++) {
+//     console.log(num = i **3);
+// }
+
+// // 10-masala
+// let sum = 0;
+// for (let i = 0; i <= 1000; i++) {
+//     sum +=i;
+// }
+// console.log(sum);
+
+// // 11-masala
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i + "." + "Salom");
+// }
+
+// // 12-masala
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i * 2);
+// }
+
+// // 13-masala
+// let sum = 1;
+// for (let i = 0; i <= 10; i++) {
+//     sum = 2 ** i;
+//     console.log("2ning " + i + "- darajasi: " + sum);
+// }
+
+// // 14-masala
+// let num  = 5;
+// let degree = 2;
+// let res;
+// for (let i = 1; i <= degree; i++) {
+//     console.log(res = num ** i);
+// }
+
+// // 15-masala 
+// for (let i = 97; i <= 122; i++) {
+//     console.log(String.fromCharCode(i));
+// }
+
+
+
+// _________________/ Boolean ga oid masalalar \_________________
+// // 1-masala
+// let A = 11;
+// let B = 8;
+// let res = (A % 2 === 0 && B % 2 === 0) || (A % 2 !== 0 && B % 2 !== 0);
+// console.log(res);
+
+// // 2-masala
+// let A = 2;
+// let B = -7;
+// let C = 5;
+// let res = A > 0 && B > 0 && C > 0;
+// console.log(res);
+
+// // 3-masala
+// let A = 2;
+// let B = -7;
+// let C = -5;
+// let res = A > 0 || B > 0 || C > 0;
+// console.log(res);
+
+// // 4-masala
+// let A = -2;
+// let B = -7;
+// let C = 5;
+// let res = (A < 0 && B < 0 && C > 0) || (A > 0 && B < 0 && C < 0) || (A < 0 && B > 0 && C < 0);
+// console.log(res);
+
+// // 5-masala
+// let A = 2;
+// let B = -7;
+// let C = 5;
+// let res = (A < 0 && B > 0 && C > 0) || (A > 0 && B < 0 && C > 0) || (A > 0 && B > 0 && C < 0);
+// console.log(res);
+
+// // 6-masala
+// let num = 10;
+// res = Math.trunc(num / 100) === 0 && num % 100 >= 10; 
+// console.log(res);
+
+
+
+
+
